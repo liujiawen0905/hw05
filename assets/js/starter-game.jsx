54160be01bdd68bb1cb65cd4e02c6d437956c587
Jiawen Liu <github.com/liujiawen0905/hw03.git>
2019-02-02 21:00:14 -0500

    hw04

--- assets/js/starter-game.jsx
+++ assets/js/starter-game.jsx
@@ -9,39 +9,137 @@ export default function game_init(root) {
 class Starter extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { left: false };
+    this.state = {
+      gameBoard: this.initTiles(),
+      gameStatus: false,
+      tempValue: null,
+      tempID: null
+    }
+    this.initTiles = this.check_match.bind(this);
+    this.check_match = this.check_match.bind(this);
+    // console.log(this.state);
+
   }
 
-  swap(_ev) {
-    let state1 = _.assign({}, this.state, { left: !this.state.left });
-    this.setState(state1);
+  initTiles(){
+    let data = [];
+    for(let i = 'A'.charCodeAt(); i <='H'.charCodeAt(); i++){
+      let t1 = {
+        isHidden: true,
+        hasMatched: false,
+        value: String.fromCharCode(i),
+      }
+
+      let t2 = {
+        isHidden: true,
+        hasMatched: false,
+        value: String.fromCharCode(i),
+      }
+      data.push(t1);
+      data.push(t2);
+    }
+    data =_.shuffle(data);
+    return data;
   }
 
-  hax(_ev) {
-    alert("hax!");
+  renderTile(i){
+    return(
+      <Tile
+      id={i}
+      value={this.state.gameBoard[i].value}
+      isHidden={this.state.gameBoard[i].isHidden}
+      hasMatched={this.state.gameBoard[i].hasMatched}
+      ClickOnTile={this.check_match}
+      />
+    );
   }
 
   render() {
-    let button = <div className="column" onMouseMove={this.swap.bind(this)}>
-      <p><button onClick={this.hax.bind(this)}>Click Me</button></p>
-    </div>;
+    return(
+      <div>
+        <div className="row">Memory Game</div>
+        <div className="row">
+          {this.renderTile(0)}
+          {this.renderTile(1)}
+          {this.renderTile(2)}
+          {this.renderTile(3)}
+        </div>
+        <div className="row">
+          {this.renderTile(4)}
+          {this.renderTile(5)}
+          {this.renderTile(6)}
+          {this.renderTile(7)}
+        </div>
+        <div className="row">
+          {this.renderTile(8)}
+          {this.renderTile(9)}
+          {this.renderTile(10)}
+          {this.renderTile(11)}
+        </div>
+        <div className="row">
+          {this.renderTile(12)}
+          {this.renderTile(13)}
+          {this.renderTile(14)}
+          {this.renderTile(15)}
+        </div>
+      </div>
+    );
+  }
 
-    let blank = <div className="column">
-      <p>Nothing here.</p>
-    </div>;
+  check_match(id){
+    let v = this.state.gameBoard[id].value;
+    let t = this.state.gameBoard;
+    t[id].isHidden = false;
+    console.log(this.state.gameBoard[id]);
 
-    if (this.state.left) {
-      return <div className="row">
-        {button}
-        {blank}
-      </div>;
+    if(this.state.tempValue != null){
+      if(this.state.tempValue == v){
+        t[id].isHidden = false;
+        t[id].hasMatched = true;
+        this.setState({gameBoard: t, tempValue: null});
+      }
+      else {
+        // console.log("here");
+        setTimeout(()=> {
+          t[id].isHidden = true;
+          t[this.state.tempID].isHidden = true;
+          this.setState({gameBoard: t, tempValue:null, tempID:null});
+        }, 1000);
+      }
     }
-    else {
-      return <div className="row">
-        {blank}
-        {button}
-      </div>;
+    else{
+      this.setState({gameBoard:t, tempValue: v, tempID: id});
     }
   }
 }
 
+
+class Tile extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  handleClick(){
+    if(this.props.isHidden){
+      this.props.ClickOnTile(this.props.id);
+    }
+
+  }
+
+  render (){
+    if(this.props.isHidden){
+      return (<button className="tile" onClick={this.handleClick.bind(this)}>
+      </button>);
+    }
+    else if (this.props.hasMatched) {
+      return (<button className="tile">
+      {this.props.value}
+      </button>);
+    }
+    else {
+        return(<button className="tile">
+        {this.props.value
+        }</button>);
+    }
+  }
+}
